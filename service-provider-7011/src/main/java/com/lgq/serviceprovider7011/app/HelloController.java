ea5b03327a542796f2819b10b2a8811932af1cdd
ColaCW <2629080132@qq.com>
2019-05-10 17:08:49 +0800

    zuul

--- service-provider-7011/src/main/java/com/lgq/serviceprovider7011/app/HelloController.java
+++ service-provider-7011/src/main/java/com/lgq/serviceprovider7011/app/HelloController.java
@@ -1,5 +1,7 @@
 package com.lgq.serviceprovider7011.app;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,8 +13,17 @@ public class HelloController {
     @Value("${server.port}")
     String port;
 
+    private static Logger logger = LoggerFactory.getLogger(HelloController.class);
+
     @RequestMapping("/hello")
     public String index(@RequestParam String name) {
-        return "hello "+name+"，this is from port:" + port;
+        logger.info("request two name is "+name);
+        try{
+            Thread.sleep(1000000);
+        }catch ( Exception e){
+            logger.error(" hello two error",e);
+        }
+        return "hello "+name+"，this is two messge";
+//        return "hello "+name+"，this is from port:" + port;
     }
 }
